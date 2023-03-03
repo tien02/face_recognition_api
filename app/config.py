48ebbf73c12a1b5ec3abe4470be10815da15d425
Tien Dang <tiendang@Tiens-MacBook-Pro.local>
2023-03-03 19:53:36 +0700

    update file available

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ DETECTOR_ID = 0
 
 ### Data Config
 DB_PATH = "app/data"  # relative path to images database (in this case is a folder of image)
-RESIZE = False
+RESIZE = True
 SIZE = (300, 300)
 
 #### -----------------!!!----------------- ###
--- app/main.py
+++ app/main.py
@@ -1,42 +1,50 @@
 import os 
+import shutil
 import app.config as config
 from PIL import Image
 from deepface import DeepFace
 from app.utils import remove_representation
-from fastapi import FastAPI, Query, HTTPException
+from fastapi import FastAPI, Query, HTTPException, File, UploadFile
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {
-        "message": "Welcome to Face Recognition API!"
+        "message": "Welcome to Face Recognition API."
     }
 
-@app.get("/face-recognition")
-def face_recognition(query_img_path:str=Query(..., description="Path to the query image file")) -> str:
+@app.post("/recognition/")
+def face_recognition(
+    img_file:UploadFile =  File(...,description="Query image file"),
+    return_image_name:bool = Query(default=True, description="Whether return only image name or full image path"),
+) -> str:
 
     '''
     Do Face Recognition task, give the image which is 
     the most similar with the input image from the 
     database - in this case is a folder of images
 
-    Argument:
-        query_img_path (str) Path to the query image (e.g: images/img1.jpeg)
-    Return:
-        value_img_path (str) Path to the value image
+    Return path to the most similar image file
     '''
     
-    if not os.path.exists(query_img_path):
-        raise HTTPException(status_code=404, detail='Path to the image is not exist!')
-    
+    if not os.path.exists("query"):
+        os.makedirs("query")
+
+    query_img_path = os.path.join("query", img_file.filename)
+
+    with open(query_img_path, "wb") as w:
+        shutil.copyfileobj(img_file.file, w)
+
     df = DeepFace.find(img_path=query_img_path, 
-                        db_path=config.DB_PATH, 
-                        model_name=config.MODELS[config.MODEL_ID], 
-                        distance_metric=config.METRICS[config.METRIC_ID], 
-                        detector_backend=config.DETECTORS[config.DETECTOR_ID], 
-                        silent=True)
+                        db_path = config.DB_PATH, 
+                        model_name = config.MODELS[config.MODEL_ID], 
+                        distance_metric = config.METRICS[config.METRIC_ID], 
+                        detector_backend = config.DETECTORS[config.DETECTOR_ID], 
+                        silent = True, align = True, prog_bar = False)
     
+    os.remove(query_img_path)
+
     if not df.empty:
         path_to_img, metric = df.columns
         ascending = True
@@ -45,43 +53,66 @@ def face_recognition(query_img_path:str=Query(..., description="Path to the quer
         df = df.sort_values(by=[metric], ascending=ascending)
         value_img_path = df[path_to_img].iloc[0]
 
-        return value_img_path
+        if return_image_name:
+            return_value = value_img_path.split(os.path.sep)[-1]
+            return_value = return_value.split(".")[0]
+            return return_value
+        else:
+            return value_img_path
     else:
         return "No Image Found"
 
-@app.post('/face_register')
-def face_register(input_path:str = Query(..., description="Path to input image"), img_name: str=Query(..., description="Image name")):
+@app.post('/register')
+def face_register(
+    img_file: UploadFile = File(..., description="Upload Image"),
+    img_save_name: str | None = Query(
+        default=None,
+        description="File's name to be save, file extension can be available or not",
+    ),):
     '''
-    Add new user to the database for Face Recognition task 
-    by registering. Resize image if necessary.
-
-    Arguments:
-        input_path (str) Path to the input image. (e.g: images/img1.jpeg)
-        img_name (str) Image's name, expect to be unique in the database. (e.g: im2)
+    Add new user to the database by face registering. Resize image if necessary.
     '''
-    if not os.path.exists(input_path):
-        raise HTTPException(status_code=404, detail='Path to the image is not exist!')
+    if img_save_name is not None:
 
-    save_img_path = img_name
-    if "." not in img_name:
-        extension = input_path.split(".")[1]
-        save_img_path = save_img_path + "." + extension
-    save_img_path = os.path.join(config.DB_PATH,save_img_path)
-    if os.path.exists(save_img_path):
-        raise HTTPException(status_code=409, detail=f"{save_img_path} already in the database.")
+        extension = img_file.filename.split(".")[-1]
+
+        if "." in img_save_name:
+            img_save_name_extension = img_save_name.split(".")[-1]
+            if extension != img_save_name_extension:
+                raise HTTPException(status_code=404, detail='File extension should match')
+            save_img_dir = os.path.join(config.DB_PATH, img_save_name)
+
+        else:
+            save_img_dir = os.path.join(config.DB_PATH, img_save_name + "." + extension)
+
+
+    if os.path.exists(save_img_dir):
+        raise HTTPException(status_code=409, detail=f"{save_img_dir} has already in the database.")
     
-    image = Image.open(input_path)
     if config.RESIZE:
-        image = image.resize(config.SIZE)
+        try:
+            image = Image.open(img_file.file)
+            if image.mode in ("RGBA", "P"):
+                image = image.convert("RGB")
+            image = image.resize(config.SIZE)
+            image.save(save_img_dir)
+        except:
+            raise HTTPException(status_code=500, detail="Something went wrong when saving the image")
+        finally:
+            img_file.file.close()
+            image.close()
+  
+    else:
+        with open(save_img_dir, "wb") as w:
+            shutil.copyfileobj(img_file.file, w)
 
-    image = image.save(save_img_path)
     remove_representation() # delete all representation_*.pkl created by DeepFace.find
     return {
-        "message": f"Image has been saved at {save_img_path}"
-        }
+        "message": f"{img_file.filename} has been save at {save_img_dir}.",
+    }
     
 
-@app.put('/change-image-name')
+@app.put('/change-file-name')
 def change_img_name(src_path:str = Query(..., description="File image going to be change"), img_name:str = Query(..., description="New name")):
     '''
     Change file name in database
@@ -109,10 +140,10 @@ def change_img_name(src_path:str = Query(..., description="File image going to b
         "message": f"Already change {src_path} file name to {new_path}"
     }
 
-@app.delete('/delete')
+@app.delete('/delete-single-image')
 def del_img(img_path:str = Query(..., description="Path to the image need to be deleted")):
     '''
-    Delete Image from image path
+    Delete single image file in database
 
     Arguments:
         img_path (str) Path to the image (e.g: images/img1.jpeg)
@@ -123,5 +154,20 @@ def del_img(img_path:str = Query(..., description="Path to the image need to be 
     os.remove(img_path)
 
     return {
-        "message": f"{img_path} has already been deleted"
+        "message": f"{img_path} has already been deleted!"
     }
+
+@app.delete('/delete-all-image')
+def del_db():
+    '''
+    Delete all image file in database ~ Delete database
+    '''
+    for img in os.listdir(config.DB_PATH):
+        os.remove(img)
+    
+    if not os.listdir():
+        return {
+            "message": "All file have been deleted!"
+        }
+    else:
+        raise HTTPException(status_code=500, detail="Some thing wrong happened.")
